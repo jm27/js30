31f8a4a4cb07321a8f03d064a7c38a80e4ad143b
Jesus Esquer <jme2791@gmail.com>
2020-10-12 13:25:42 -0700

    Added playsound function

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # js30-drumKit
+
+* Key events
+* Data-key
+* audio.play()
+* audio.currentTime = 0
+* transition
--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-window.addEventListener("keydown", function (e) {
+
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  console.log(key);
   if (!audio) return; // stop function from running
   audio.currentTime = 0; // rewind to start
   audio.play();
   key.classList.add("playing");
-});
+}
 
 function removeTransition(e) {
-    console.log(e)
+  if (e.propertyName !== "transform") return; // if it is not transform return
+  this.classList.remove("playing");
 }
 
 const keys = document.querySelectorAll(".key");
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
-
+// Event listener
+window.addEventListener("keydown", playSound);
