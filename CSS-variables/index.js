efcd237736ec553021553821b534f787ee0b62e8
Jesus Esquer <jme2791@gmail.com>
2020-10-14 13:51:10 -0700

    Updating CSS variable successfully

--- CSS-variables/index.js
+++ CSS-variables/index.js
@@ -1,1 +1,15 @@
-console.log("hello");
+// Select all inputs in the page
+const inputs = document.querySelectorAll(".controls input");
+
+// Function to handle updates
+function handleUpdate() {
+  console.log(this.value);
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+}
+// Listen to update on each input
+inputs.forEach((input) => input.addEventListener("change", handleUpdate));
+inputs.forEach((input) => input.addEventListener("mousemove", handleUpdate));
--- README.md
+++ README.md
@@ -12,3 +12,11 @@
 * CSS transform, transition, transform-origin, transition-timing-function
 * getSeconds(), getMinutes(), getHours()
 * setInterval()
+
+# js30-Update-CSS-Variables
+
+* :root
+* CSS Variable, declare: --name, use: var(--name).
+* dataset: Object contains all dataset attributes.
+* document.documentElement.style.setProperty(`--varname`, updatename).
+* Event Listeners: mousemove, change.
