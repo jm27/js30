577092b6cb6a9a7618209e4251c1156298699c30
Jesus Esquer <jme2791@gmail.com>
2020-10-15 20:44:55 -0700

    Panels classes toggle working

--- Flex-Panels-Img/index.js
+++ Flex-Panels-Img/index.js
@@ -1,1 +1,19 @@
-console.log('ready');
+// Select all elements with class name panels
+const panels = document.querySelectorAll(".panel");
+// Create toggle function for class open
+function toggleOpen() {
+  this.classList.toggle("open");
+}
+// Creat funtion for toggle class Active
+function toggleActive(e) {
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+  //   console.log(e.propertyName);
+}
+// Add event listener to each panel element
+panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+// Add event listener for transition of element
+panels.forEach((panel) =>
+  panel.addEventListener("transitionend", toggleActive)
+);
