9fe01f02778efc02f3d464abe9b3cca48b0dab4b
Jesus Esquer <jme2791@gmail.com>
2020-10-16 18:03:03 -0700

    Array exercises 1-5 completed

--- Array-Cardio/index.js
+++ Array-Cardio/index.js
@@ -64,17 +64,39 @@ const people = [
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
+const fifteeners = inventors.filter(
+  (inventor) => inventor.year > 1500 && inventor.year <= 1600
+);
+console.log(fifteeners);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
+const fullNames = inventors.map(
+  (inventor) => `${inventor.first}, ${inventor.last}`
+);
+
+console.log(fullNames);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
+const sortedInv = inventors.sort((a, b) => (a.year > b.year ? 1 : -1));
+console.table(sortedInv);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
+const yearsSum = inventors.reduce(
+  (sum, inv) => (sum = inv.passed - inv.year + sum),
+  0
+);
+
+console.log(yearsSum);
 
 // 5. Sort the inventors by years lived
+const sortedByYears = inventors.sort((a, b) =>
+  a.passed - a.year > b.passed - b.year ? -1 : 1
+);
+
+console.table(sortedByYears);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
