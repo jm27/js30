0084bca6da687688ccfbded06528af002846af32
Jesus Esquer <jme2791@gmail.com>
2020-10-17 19:17:32 -0700

    Filter by city or state working

--- AJAX-Type-Ahead/index.js
+++ AJAX-Type-Ahead/index.js
@@ -1,2 +1,21 @@
-console.log('working!')
-const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+// JSON file with Cities
+const endpoint =
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+// Save cities in an Array
+const cities = [];
+
+// Get cities from Url
+fetch(endpoint)
+  .then((blob) => blob.json())
+  .then((data) => cities.push(...data));
+
+// function to filter cities
+function findMatches(wordToMatch, cities) {
+  return cities.filter((place) => {
+    // Create Regex to match word
+    const regex = new RegExp(wordToMatch, "gi"); // global, insensitive
+    // Check if city or state matches what is searched
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
