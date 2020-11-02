64102ba5fd8feb579b8fd1029ebb88839e404b4e
Jesus Esquer <jme2791@gmail.com>
2020-11-01 18:54:56 -0700

    Added delete all, check all and uncheck all bonus

--- LocalStorage/index.js
+++ LocalStorage/index.js
@@ -3,7 +3,11 @@ const addItems = document.querySelector(".add-items");
 // Plates div
 const itemsList = document.querySelector(".plates");
 // Store Data
-const items = [];
+const items = JSON.parse(localStorage.getItem("items")) || [];
+// Select buttons
+const delBtn = document.querySelector(".delete");
+const checkAllBtn = document.querySelector(".checkAll");
+const unCheckAllBtn = document.querySelector(".unCheckAll");
 
 function addItem(e) {
   e.preventDefault();
@@ -14,13 +18,14 @@ function addItem(e) {
   };
   items.push(item);
   populateList(items, itemsList);
+  localStorage.setItem("items", JSON.stringify(items));
   this.reset();
   console.log(items);
 }
 
 function populateList(items = [], platesList) {
   platesList.innerHTML = items
-    .map((plate, index) => {
+    .map((plate, i) => {
       return `
     <li>
     <input type="checkbox" data-index=${i} id="item${i}" ${
@@ -33,4 +38,35 @@ function populateList(items = [], platesList) {
     .join("");
 }
 
+function toggleDone(e) {
+  if (!e.target.matches("input")) return;
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+function deleteAll(e) {
+  console.log("click");
+  items.length = 0;
+  localStorage.removeItem("items");
+  populateList(items, itemsList);
+}
+function checkAll(e) {
+  items.map((item) => (item.done = true));
+  localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemsList);
+}
+function unCheckAll(e) {
+  items.map((item) => (item.done = false));
+  localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
 addItems.addEventListener("submit", addItem);
+itemsList.addEventListener("click", toggleDone);
+delBtn.addEventListener("click", deleteAll);
+checkAllBtn.addEventListener("click", checkAll);
+unCheckAllBtn.addEventListener("click", unCheckAll);
+populateList(items, itemsList);
