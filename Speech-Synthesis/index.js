5a594ccdc8f74e08874f4e40def8523412a4c171
Jesus Esquer <jme2791@gmail.com>
2020-11-14 16:14:25 -0700

    Voiceinator 5000 working

--- Speech-Synthesis/index.js
+++ Speech-Synthesis/index.js
@@ -2,5 +2,43 @@ const msg = new SpeechSynthesisUtterance();
 let voices = [];
 const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
-const speakButton = document.querySelector('#speak');
-const stopButton = document.querySelector('#stop');
+const speakButton = document.querySelector("#speak");
+const stopButton = document.querySelector("#stop");
+
+// Select box for input
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+    .map(
+      (voice) =>
+        `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`
+    )
+    .join("");
+  console.log(voices);
+}
+
+function setVoice() {
+  msg.voice = voices.find((voice) => voice.name === this.value);
+}
+
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if (startOver) {
+    speechSynthesis.speak(msg);
+  }
+}
+
+function setOption() {
+  msg[this.name] = this.value;
+  toggle();
+}
+
+// Take global variable for speech
+speechSynthesis.addEventListener("voiceschanged", populateVoices);
+voicesDropdown.addEventListener("change", setVoice);
+// Select options and listen for change
+options.forEach((option) => option.addEventListener("change", setOption));
+speakButton.addEventListener("click", toggle);
+stopButton.addEventListener("click", toggle.bind(null, false));
