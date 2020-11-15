f19bfa94d66156bb22d4155279174f58d72a43fc
Jesus Esquer <jme2791@gmail.com>
2020-11-15 13:53:23 -0700

    Sticky nav class toggle on scroll

---
+++ Sticky-Nav/index.js
@@ -0,0 +1,14 @@
+//Select navbar
+
+const nav = document.querySelector("#main");
+const topOfNav = nav.offsetTop;
+
+function fixNav() {
+  if (window.scrollY >= topOfNav) {
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.classList.remove("fixed-nav");
+  }
+}
+
+window.addEventListener("scroll", fixNav);
