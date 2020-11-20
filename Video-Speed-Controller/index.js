f91299e13b5c26358ddc816e3cc33ace3daeb275
Jesus Esquer <jme2791@gmail.com>
2020-11-19 19:31:54 -0700

    speed bar working

--- Video-Speed-Controller/index.js
+++ Video-Speed-Controller/index.js
@@ -3,8 +3,8 @@ const speed = document.querySelector(".speed");
 const bar = speed.querySelector(".speed-bar");
 const video = document.querySelector(".flex");
 
-// Add event listener for mouse move
-speed.addEventListener("mousemove", function (e) {
+//Handle move function
+function handleMove(e) {
   const y = e.pageY - this.offsetTop;
   const percent = y / this.offsetHeight;
   const min = 0.4;
@@ -12,4 +12,10 @@ speed.addEventListener("mousemove", function (e) {
 
   const height = Math.round(percent * 100) + "%";
   bar.style.height = height;
-});
+  const playbackRate = percent * (max - min) + min;
+  bar.textContent = playbackRate.toFixed(2) + "x";
+  video.playbackRate = playbackRate;
+}
+
+// Add event listener for mouse move
+speed.addEventListener("mousemove", handleMove);
