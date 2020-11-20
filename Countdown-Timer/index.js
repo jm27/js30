cdf8b7d3db143285cc6f371bf3c2ce258e04f934
Jesus Esquer <jme2791@gmail.com>
2020-11-20 16:43:59 -0700

    getting end time

--- Countdown-Timer/index.js
+++ Countdown-Timer/index.js
@@ -1,12 +1,16 @@
 // Store countdown in variable
 let countdown;
+// Select display time
+const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
 
 // make function for timer
 function timer(seconds) {
   //when the timer started
   const now = Date.now();
   const then = now + seconds * 1000;
-  displayTimeLeft(secondLeft);
+  displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondLeft = Math.round((then - Date.now()) / 1000);
@@ -24,5 +28,19 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${
+    remainderSeconds < 10 ? "0" : ""
+  } ${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
   console.log(seconds);
+}
+
+// Display end time
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be back at ${hours}:${minutes}`;
 }
