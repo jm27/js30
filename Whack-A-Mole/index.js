b3217ea524294e7af24877b94e2c212e75ceaccc
Jesus Esquer <jme2791@gmail.com>
2020-11-21 20:32:31 -0700

    Whack a mole working changing class

--- Whack-A-Mole/index.js
+++ Whack-A-Mole/index.js
@@ -1,3 +1,30 @@
+// Select hole, score and moles
 const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
+let lastHole;
+
+// Create a function with random amount of time
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+// Pick a random hole
+function randomHole(holes) {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  if (hole === lastHole) {
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
+
+function peep() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add("up");
+  setTimeout(() => {
+    hole.classList.remove("up");
+  }, time);
+}
