d92d1fa09d6c80c3ffc3339eb010072976036df8
Jesus Esquer <jme2791@gmail.com>
2020-11-22 13:37:10 -0700

    Getting jokes from api and loader working

--- Dad-Jokes/index.js
+++ Dad-Jokes/index.js
@@ -1,6 +1,8 @@
 // Get joke button and holder
 const jokeButton = document.querySelector(".getJoke");
 const jokeHolder = document.querySelector(".joke p");
+// Select loader
+const loader = document.querySelector(".loader");
 
 // Button text
 const buttonText = [
@@ -16,19 +18,35 @@ const buttonText = [
 
 // fetch jokes function
 async function fetchJoke() {
-  response = await fetch("https://icanhazdadjoke.com", {
+  // turn loader on
+  loader.classList.remove("hidden");
+  const response = await fetch("https://icanhazdadjoke.com", {
     headers: {
       Accept: "application/json",
     },
   });
   const data = await response.json();
+  // turn loader off
+  loader.classList.add("hidden");
   return data;
+}
+// Change button text
+function randomItemFromArray(arr, not) {
+  const item = arr[Math.floor(Math.random() * arr.length)];
+  if (item === not) {
+    return randomItemFromArray(arr, not);
+  }
+  return item;
 }
 
 // When get joke is click fetch joke
 async function handleClick() {
   const { joke } = await fetchJoke();
   jokeHolder.textContent = joke;
+  jokeButton.textContent = randomItemFromArray(
+    buttonText,
+    jokeButton.textContent
+  );
 }
 
 // Listen for click on get joke button
